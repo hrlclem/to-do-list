a6292fa939c50555695d5769b3d344f3afcd7450
Clement <perso@clementhurel.fr>
2022-06-30 12:23:23 +0900

    MY PROJECTS main task management

--- src/tasksFunctions.js
+++ src/tasksFunctions.js
@@ -65,7 +65,7 @@ export function confirmAddTask(e) {
     showAddTask.style.display = "flex";
 
     if (taskName === "") {
-      alert("Task  can't be empty");
+      alert("Task can't be empty");
       return;
     }
 
